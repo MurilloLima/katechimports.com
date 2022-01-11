49f15cca62c41cc6a9d09d794367f4558d709269
Murillo Lima <murillo_smit@hotmail.com>
2022-01-11 19:15:46 -0300

    edit footer

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2021-{{date('Y')}} <a href="{{ route('admin.index') }}">+SS Saúde e Consultoria.</a></strong>
+    <strong>Copyright &copy; 2021-{{date('Y')}} <a href="{{ route('admin.index') }}">Katech Imports.</a></strong>
     Todos os direitos reservados.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0.0
